59c23bfd0f8145effb1ce82c2414b726d1395b31
JETSADA UDOMDIREKRIRK (CDGS-ADD) <jetsada.u@cdg.co.th>
2021-10-01 13:45:37 +0700

    add count bean to spring

--- src/main/java/com/example/day02/Day02Application.java
+++ src/main/java/com/example/day02/Day02Application.java
@@ -2,12 +2,18 @@ package com.example.day02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Day02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day02Application.class, args);
+		ConfigurableApplicationContext ctx =  SpringApplication.run(Day02Application.class, args);
+		System.out.println(ctx.getBeanDefinitionCount());
+//		for (String name : ctx.getBeanDefinitionNames()) {
+//			System.out.println(name);
+//		}
+			
 	}
 
 }
